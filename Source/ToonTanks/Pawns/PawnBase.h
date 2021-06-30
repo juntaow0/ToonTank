ad83b6d3f9e4c0896ceef4eba82528dd986fa142
juntaow0 <juntaowang1998@outlook.com>
2021-06-29 23:31:00 -0700

    tank and turret base

---
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PawnBase.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS()
+class TOONTANKS_API APawnBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APawnBase();
+
+private:
+	// for collision
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components", meta=(AllowPrivateAccess="true"))			
+	UCapsuleComponent* CapsuleComp = nullptr;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* BaseMesh = nullptr;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* TurretMesh = nullptr;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components", meta=(AllowPrivateAccess="true"))
+	USceneComponent* ProjectileSpawnPoint = nullptr;
+};
